8af1368a20076444845f992211af0f153f3a602e
Edlingao <sonido244@gmail.com>
2022-03-26 21:53:42 +0000

    feat(rootComponent): adding the template for root component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { IndexComponent } from './views/movies/movies.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'movies',
+    component: IndexComponent,
+  },
+  {
+    path: 'contact',
+
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
