945cbb8fb0a004c8829ef89b02c491e8ba298147
Edlingao <sonido244@gmail.com>
2022-03-26 21:49:11 +0000

    feat(view): adding the movie view
    This commit adds the movie view

--- src/app/views/movies/movies.component.spec.ts
+++ src/app/views/movies/movies.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { IndexComponent } from './index.component';
+import { IndexComponent } from './movies.component';
 
 describe('IndexComponent', () => {
   let component: IndexComponent;
