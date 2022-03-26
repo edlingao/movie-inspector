63ab7fc1943d771df1d123bf0cdf432b7fbdb1fc
Edlingao <sonido244@gmail.com>
2022-03-26 21:54:41 +0000

    feat(component): adding table component
    This commits adds table component

---
+++ src/app/components/table/table.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.scss']
+})
+export class TableComponent implements OnInit {
+  
+  @Input() movies:Array<any> = [];
+  
+  displayedColumns: string[] = ['title', 'year', 'genres', 'cast']
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
