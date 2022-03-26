48eee7ce90961181b58759b4ba40ddad02945225
Edlingao <sonido244@gmail.com>
2022-03-26 21:58:52 +0000

    fix(routes): fixing routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './views/contact/contact.component';
 import { HomeComponent } from './views/home/home.component';
 import { IndexComponent } from './views/movies/movies.component';
 
@@ -14,7 +15,7 @@ const routes: Routes = [
   },
   {
     path: 'contact',
-
+    component: ContactComponent
   }
 ];
 
