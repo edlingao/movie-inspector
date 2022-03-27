9db21063ae342952f7ed71134323db5eda30ca37
Edlingao <sonido244@gmail.com>
2022-03-27 11:43:11 +0000

    feat(form): adding form
    This commit adds form component

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormFieldsService } from 'src/app/services/form-fields.service';
+import { Submit } from 'src/app/types';
+
 
 @Component({
   selector: 'app-form',
@@ -6,10 +10,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.scss']
 })
 export class FormComponent implements OnInit {
+  @Input() maxChars: number = 5
+  @Output() onValidEvent = new EventEmitter<Boolean>();
+  @Output() onSubmitEvent = new EventEmitter<Submit>();
 
-  constructor() { }
+  myForm: FormGroup = new FormGroup({});
+  fields: Array<any> = [];
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private formFields:FormFieldsService
+  ) { 
+    this.fields = formFields.getFields()
+  }
 
   ngOnInit(): void {
+    const commonValidators = [Validators.required];
+    const nameRegx = new RegExp(/^[a-zA-Z0-9\s]*$/im);
+    const numberValidator = new RegExp(/^\+([0-9])+/im);
+    this.myForm = this.formBuilder.group({
+      name: ['', [...commonValidators, Validators.pattern(nameRegx)]],
+      email: ['', [...commonValidators, Validators.email]],
+      phone: ['', [...commonValidators, Validators.pattern(numberValidator)]],
+      message: ['', [...commonValidators, Validators.maxLength(this.maxChars)]],
+    });
+  }
+
+  onSubmit(form: FormGroup): void {
+    if(form.valid) {
+      this.onSubmitEvent.emit(form.value)
+    }
+  }
+
+  capitalize(word: string): string {
+    return word[0].toUpperCase() + word.slice(1).toLowerCase();
+  }
+
+  eval(textWithVariables: string): string {
+    //Hacky but scalable :D/
+    return eval(`let maxChars = ${this.maxChars};${textWithVariables}`);
+  }
+
+  onValid(valid: Boolean) {
+    if(valid) {
+      this.onValidEvent.emit(valid)
+    }
+  }
+
+  isFieldValid(name: string): Boolean {
+    return Boolean(this.myForm.get(name)?.invalid && ( 
+      this.myForm.get(name)?.dirty 
+      || this.myForm.get(name)?.touched
+    ))
   }
 
 }
--- src/app/modules/reactive-forms-module/reactive-forms-module.module.ts
+++ src/app/modules/reactive-forms-module/reactive-forms-module.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ReactiveFormsModule } from '@angular/forms';
 import { FormComponent } from '../../components/form/form.component'
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { UploadFormService } from '../../services/upload-form.service';
 
 @NgModule({
   declarations: [ FormComponent ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatButtonModule
   ],
-  exports: [ FormComponent ]
+  exports: [ FormComponent ],
+  providers: [ UploadFormService ]
 })
 export class ReactiveFormsModuleModule { }
